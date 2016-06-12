f50989950a8089c56a3c7ca9ca9979129a6f34e3
Todor Davchev <t.davchev@gmail.com>
2016-06-12 20:52:48 +0100

    SNLI preprocess

--- data/scripts/process_snli.py
+++ data/scripts/process_snli.py
@@ -4,11 +4,19 @@ script, filename = argv
 
 target = open(filename, 'r')
 
+idx = -1
+# skip the first row
 first = 1
 for line in target:
 	if first < 1:
 		elements = line.split('\t')
-		print("{0}\t{1}\t{2}".format(elements[5],elements[6],elements[0]))
+		if elements[0]=='entailment':
+			idx = 0
+		elif elements[0]=='neutral':
+			idx = 1
+		else:
+			idx = 2
+		print("{0}\t{1}-{2}".format(idx,elements[5],elements[6]))
 	else:
 		first = 0	
 
