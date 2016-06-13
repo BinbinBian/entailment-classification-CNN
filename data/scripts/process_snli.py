36e26bc8c385c3f31c8d5ab7fb0338fe1c703121
Todor Davchev <t.davchev@gmail.com>
2016-06-13 13:30:49 +0100

    new model data preprocessing

--- data/scripts/process_snli.py
+++ data/scripts/process_snli.py
@@ -1,6 +1,6 @@
 from sys import argv
 
-script, filename = argv
+script, filename, model = argv
 
 target = open(filename, 'r')
 
@@ -16,8 +16,13 @@ for line in target:
 			idx = 1
 		else:
 			idx = 2
-		print("{0}\t{1}-{2}".format(idx,elements[5],elements[6]))
+		if model == 1:
+			print("{0}\t{1}-{2}".format(idx,elements[5],elements[6]))
+		elif model == 21: # record only first sentence and the element
+			print("{0}\t{1}".format(idx,elements[5]))
+		elif model == 22:
+			print("{0}\t{2}".format(idx,elements[6]))
 	else:
-		first = 0	
+		first = 0
 
 target.close()
